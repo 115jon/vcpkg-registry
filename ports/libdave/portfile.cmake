cc40b30001b6b2f759f2e1c5477cce5f55b08f29
115jon <115jon@proton.me>
2026-01-08 12:46:56 -0600

    fix: 🐛 use explicit commit refs in portfiles instead of VERSION variable

--- ports/libdave/portfile.cmake
+++ ports/libdave/portfile.cmake
@@ -1,31 +1,31 @@
-vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
-    FEATURES
-        disable-logging DISABLE_LOGGING
-)
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS FEATURES
+                     disable-logging DISABLE_LOGGING)
 
 set(PATCHES "vcpkg-support.patch")
 if(DISABLE_LOGGING)
-    list(APPEND PATCHES "disable-logging.patch")
+  list(APPEND PATCHES "disable-logging.patch")
 endif()
 
 vcpkg_from_github(
-    OUT_SOURCE_PATH SOURCE_PATH
-    REPO discord/libdave
-    REF "${VERSION}"
-    SHA512 6284ba5842519eb3be64bd2dd98dacdd75443738ca1b5b5a0642d2b21576cc57b28b0e0cf517654efd2b22af7160d818f951404132069f5907c5dc528f735165
-    PATCHES ${PATCHES}
-)
+  OUT_SOURCE_PATH
+  SOURCE_PATH
+  REPO
+  discord/libdave
+  REF
+  c0cf6449724082488aeb9d9c5398fe1c131abb04
+  SHA512
+  6284ba5842519eb3be64bd2dd98dacdd75443738ca1b5b5a0642d2b21576cc57b28b0e0cf517654efd2b22af7160d818f951404132069f5907c5dc528f735165
+  HEAD_REF
+  main
+  PATCHES
+  ${PATCHES})
 
-vcpkg_cmake_configure(
-    SOURCE_PATH "${SOURCE_PATH}/cpp"
-)
+vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}/cpp")
 
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(PACKAGE_NAME libdave CONFIG_PATH lib/cmake/libdave)
 
-vcpkg_install_copyright(
-    FILE_LIST "${SOURCE_PATH}/LICENSE"
-)
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
